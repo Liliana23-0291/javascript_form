a0ae46a7dd4e30a694394031d23788818731b3f9
liliana <lpmdavid23-0291@cca.edu.ph>
2025-02-17 19:36:46 +0800

    updated create with database

--- create.php
+++ create.php
@@ -2,7 +2,41 @@
 
     echo "Hello World";    
     var_dump($_POST);
+    $lastname = $_POST['last_name'];
+    $firstname = $_POST['first_name'];
+?>
 
+<br>
+<br>
+<br>
+<br>
 
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "lorraine";
 
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $database);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    echo "Connected successfully";
+
+    $sql = "INSERT INTO students (id, last_name,first_name)
+    VALUES ('', '$lastname', '$firstname')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "<br>";
+        echo "<br>";
+        echo "<br>";
+        echo "<br>";
+        
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
 ?>
